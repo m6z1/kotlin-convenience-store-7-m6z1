67fe5114960919d085dcd3d65adcfbabcd5a6cac
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-07 17:22:03 +0900

    feat: 구매할 상품과 수량 입력 메서드 구현
    - InputView.kt: 구매할 상품과 수량을 입력받는 메서드를 추가합니다.

---
+++ src/main/kotlin/store/InputView.kt
@@ -0,0 +1,17 @@
+package store
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+
+    fun readProductsToPurchase(): List<Map<String, Int>> {
+        println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])")
+        val inputLines = Console.readLine().split(",")
+        return inputLines.map { inputLine ->
+            val product = inputLine.replace("[", "").replace("]", "")
+            val (name, quantity) = product.split("-")
+
+            mapOf(name to quantity.toInt())
+        }
+    }
+}
