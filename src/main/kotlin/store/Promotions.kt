95f5c75e6a5f2f3d60d0bde561124817130a98c2
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-07 17:36:02 +0900

    feat: 오늘 날짜가 프로모션 적용 가능한 날짜인지 여부 판단하는 메서드 추가
    - Promotions.kt: 프로모션 이름과 오늘 날짜를 받아와서 오늘 날짜가 프르모션 할인이 가능한지 여부를 판단하는 메서드를 추가합니다.

--- src/main/kotlin/store/Promotions.kt
+++ src/main/kotlin/store/Promotions.kt
@@ -1,6 +1,7 @@
 package store
 
 import java.io.File
+import java.time.LocalDate
 
 class Promotions {
     private val promotions: MutableList<List<String>> = emptyList<List<String>>().toMutableList()
@@ -23,5 +24,13 @@ class Promotions {
         File(path).forEachLine { productsLine.add(it) }
 
         return productsLine
+    }
+
+    fun isPossiblePromotionDiscount(promotionName: String, today: LocalDate): Boolean {
+        val promotion = promotions.firstOrNull { it[0] == promotionName } ?: return false
+
+        val startDate = LocalDate.parse(promotion[3])
+        val endDate = LocalDate.parse(promotion[4])
+        return today in startDate..endDate
     }
 }
