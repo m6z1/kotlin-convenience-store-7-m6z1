057adbef25eff80fa3e65a6b7bfd884768dfa3aa
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-07 16:53:44 +0900

    feat: 각 프로모션에 대한 정보를 리스트로 가짐
    - Promotions.kt: 프로모션 파일을 읽어와 각 프로모션에 대한 정보를 저장합니다.

---
+++ src/main/kotlin/store/Promotions.kt
@@ -0,0 +1,27 @@
+package store
+
+import java.io.File
+
+class Promotions {
+    private val promotions: MutableList<List<String>> = emptyList<List<String>>().toMutableList()
+
+    init {
+        updatePromotions()
+    }
+
+    private fun updatePromotions() {
+        val productsLine = readProductsFile()
+        val productsLineExceptTitle = productsLine.filter { it != productsLine[0] }
+        productsLineExceptTitle.forEach { product ->
+            promotions.add(product.split(","))
+        }
+    }
+
+    private fun readProductsFile(): List<String> {
+        val path = "src/main/resources/promotions.md"
+        val productsLine = emptyList<String>().toMutableList()
+        File(path).forEachLine { productsLine.add(it) }
+
+        return productsLine
+    }
+}
