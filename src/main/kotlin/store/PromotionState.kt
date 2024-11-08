7e9fc098b7b3d1363cf84c2bbe11aa983339dd7d
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-08 21:01:45 +0900

    feat: 구매할 상품에 대한 프로모션 상태 정의

---
+++ src/main/kotlin/store/PromotionState.kt
@@ -0,0 +1,8 @@
+package store
+
+enum class PromotionState(private val title: String) {
+    NONE("해당 없음"),
+    NOT_ENOUGH_STOCK("프로모션 재고 없음"),
+    ELIGIBLE_BENEFIT("더 가져오면 프로모션 적용 가능"),
+    AVAILABLE_BENEFIT("프로모션 적용 가능"),
+}
