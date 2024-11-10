02519649ed7d254d7084aada2e60c656e3318507
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-10 19:22:40 +0900

    fix: 재고 수량 초과 예외 조건 수정

--- src/main/kotlin/store/products/ProductsManager.kt
+++ src/main/kotlin/store/products/ProductsManager.kt
@@ -58,7 +58,7 @@ class ProductsManager {
             val matchingProducts = _products.filter { it.name == productName }
             require(matchingProducts.isNotEmpty()) { "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요." }
 
-            val purchasableQuantity = matchingProducts.sumOf { it.price }
+            val purchasableQuantity = matchingProducts.sumOf { it.quantity }
             require(purchasableQuantity >= quantityToPurchase) { "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요." }
         }
     }
