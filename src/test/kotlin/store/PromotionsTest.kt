79c90641946befa3df528ec562463c28ef117e06
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-11 17:21:13 +0900

    test: 프로모션 테스트 추가
    - PromotionsTest.kt: 상품명과 오늘 날짜를 통해 프로모션 할인이 가능한지 확인하는 테스트를 추가합니다.

--- src/test/kotlin/store/PromotionsTest.kt
+++ src/test/kotlin/store/PromotionsTest.kt
@@ -6,9 +6,25 @@ import org.junit.jupiter.params.provider.CsvSource
 import store.products.Product
 import store.promotion.PromotionState
 import store.promotion.Promotions
+import java.time.LocalDate
 
 class PromotionsTest {
     private val promotions = Promotions()
+
+    @ParameterizedTest
+    @CsvSource(
+        "물, 2024-01-01, false",
+        "콜라, 2024-11-11, true",
+        "사이다, 2025-01-01, false",
+    )
+    fun `프로모션 할인이 가능한지 확인한다`(
+        productName: String,
+        today: LocalDate,
+        expectedValue: Boolean,
+    ) {
+        val result = promotions.isPossiblePromotionDiscount(productName, today)
+        assertEquals(expectedValue, result)
+    }
 
     @ParameterizedTest
     @CsvSource(
