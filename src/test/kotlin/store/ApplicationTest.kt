e0800bae512880835fb8d61474736da752b34785
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-11 21:29:26 +0900

    test: 추가 구매 시 최신 재고 테스트

--- src/test/kotlin/store/ApplicationTest.kt
+++ src/test/kotlin/store/ApplicationTest.kt
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertNowTest
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
@@ -56,6 +58,65 @@ class ApplicationTest : NsTest() {
         assertSimpleTest {
             runException("[컵라면-12]", "N", "N")
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.")
+        }
+    }
+
+    @DisplayName("추가 구매 후 최신 재고 테스트")
+    @Nested
+    inner class LatestProducts {
+
+        @Test
+        fun `일반 재고 구매 후 추가 구매 시 최신 재고를 출력한다`() {
+            assertSimpleTest {
+                run("[물-1]", "N", "Y", "[물-1]", "N", "N")
+                assertThat(output()).contains(
+                    "- 콜라 1,000원 10개 탄산2+1",
+                    "- 콜라 1,000원 10개",
+                    "- 사이다 1,000원 8개 탄산2+1",
+                    "- 사이다 1,000원 7개",
+                    "- 오렌지주스 1,800원 9개 MD추천상품",
+                    "- 오렌지주스 1,800원 재고 없음",
+                    "- 탄산수 1,200원 5개 탄산2+1",
+                    "- 탄산수 1,200원 재고 없음",
+                    "- 물 500원 9개",
+                    "- 비타민워터 1,500원 6개",
+                    "- 감자칩 1,500원 5개 반짝할인",
+                    "- 감자칩 1,500원 5개",
+                    "- 초코바 1,200원 5개 MD추천상품",
+                    "- 초코바 1,200원 5개",
+                    "- 에너지바 2,000원 5개",
+                    "- 정식도시락 6,400원 8개",
+                    "- 컵라면 1,700원 1개 MD추천상품",
+                    "- 컵라면 1,700원 10개"
+                )
+            }
+        }
+
+        @Test
+        fun `프로모션 재고를 남아있는 재고 초과해서 구매 시 일반 재고에서 차감한 후 최신 재고를 출력한다`() {
+            assertSimpleTest {
+                run("[사이다-10]", "Y", "N", "Y", "[물-1]", "N", "N")
+                assertThat(output()).contains(
+                    "- 콜라 1,000원 10개 탄산2+1",
+                    "- 콜라 1,000원 10개",
+                    "- 사이다 1,000원 재고 없음 탄산2+1",
+                    "- 사이다 1,000원 5개",
+                    "- 오렌지주스 1,800원 9개 MD추천상품",
+                    "- 오렌지주스 1,800원 재고 없음",
+                    "- 탄산수 1,200원 5개 탄산2+1",
+                    "- 탄산수 1,200원 재고 없음",
+                    "- 물 500원 10개",
+                    "- 비타민워터 1,500원 6개",
+                    "- 감자칩 1,500원 5개 반짝할인",
+                    "- 감자칩 1,500원 5개",
+                    "- 초코바 1,200원 5개 MD추천상품",
+                    "- 초코바 1,200원 5개",
+                    "- 에너지바 2,000원 5개",
+                    "- 정식도시락 6,400원 8개",
+                    "- 컵라면 1,700원 1개 MD추천상품",
+                    "- 컵라면 1,700원 10개"
+                )
+            }
         }
     }
 
