5984ae42bc0fc6143868495fbffd6104836edbb9
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-11 16:18:12 +0900

    refactor: 안내 메시지 inputView로 수정

--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
 
     fun readProductsToPurchase(): List<Map<String, Int>> {
+        println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])")
         val inputLines = Console.readLine().split(COMMA_DELIMITER)
         inputLines.forEach { inputLine -> validateInputLine(inputLine) }
 
@@ -36,18 +37,22 @@ class InputView {
     }
 
     fun readMembershipState(): String {
+        println("멤버십 할인을 받으시겠습니까? (Y/N)")
         return Console.readLine()
     }
 
-    fun readAddingFreebie(): String {
+    fun readAddingFreebie(productName: String): String {
+        println("현재 ${productName}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)")
         return Console.readLine()
     }
 
-    fun readRegularPriceToPay(): String {
+    fun readRegularPriceToPay(productName: String, regularPriceToPayCount: Int): String {
+        println("현재 $productName ${regularPriceToPayCount}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)")
         return Console.readLine()
     }
 
     fun readMorePurchase(): String {
+        println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)")
         return Console.readLine()
     }
 
--- src/main/kotlin/store/view/OutputView.kt
+++ src/main/kotlin/store/view/OutputView.kt
@@ -25,28 +25,8 @@ class OutputView {
         println()
     }
 
-    fun printProductsToPurchaseMessage() {
-        println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])")
-    }
-
     fun printErrorMessage(errorMessage: String?) {
         println(errorMessage)
-    }
-
-    fun printMembershipMessage() {
-        println("멤버십 할인을 받으시겠습니까? (Y/N)")
-    }
-
-    fun printAddingFreebieMessage(productName: String) {
-        println("현재 ${productName}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)")
-    }
-
-    fun printRegularPriceToPayMessage(productName: String, regularPriceToPayCount: Int) {
-        println("현재 $productName ${regularPriceToPayCount}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)")
-    }
-
-    fun printMorePurchaseMessage() {
-        println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)")
     }
 
     fun printReceipt(
